eda6d620244132bb19247186facb18bd832743d7
Nozomu05 <155920440+Nozomu05@users.noreply.github.com>
2024-12-08 23:09:53 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 
 #partie 1
-echo "choisir que faire\n1) accepter tout les traffics\n2) refuser tout les traffics"
+echo "choisir que faire\n1) accepter tout les traffics\n2) refuser tous les traffics"
 read valeur
 
 if [ $valeur -eq 1 ]; then
@@ -14,7 +14,7 @@ elif [ $valeur -eq 2 ]; then
 	sudo iptables -P OUTPUT DROP
 	sudo iptables -P FORWARD DROP
 else
-	echo "choix invalide"
+	echo "choix invalidé"
 
 fi
 
@@ -83,13 +83,13 @@ modifier_regle() {
 supprimer_regle() {
   echo "\nSupprimer une règle :"
   iptables -L --line-numbers
-  echo "\nNuméro de la règle à supprimer  : " 
+  echo "\nNuméro de la règle a supprimer  : " 
   read numero
   iptables -D INPUT $numero
   echo "Règle supprimée."
 }
 
-echo -e "\nGestion des règles de pare-feu :"
+echo -e "\nGestion des règle de pare-feu :"
 echo "1) Ajouter une règle"
 echo "2) Modifier une règle"
 echo "3) Supprimer une règle"
@@ -116,6 +116,8 @@ configurer_nat() {
     read -p "Activer le masquage NAT (oui/non) ? : " nat
     if [ "$nat" = "oui" ]; then
         iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+    elif [ "$nat" = "non" ]; then 
+    	 iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
     fi
 
     read -p "Rediriger un port (oui/non) ? : " port
@@ -126,15 +128,22 @@ configurer_nat() {
         iptables -t nat -A PREROUTING -p tcp --dport $port_source -j DNAT --to-destination $ip_dest:$port_dest
     fi
 }
+configurer_nat
 
 #partie 4
 activer_journalisation() {
     read -p "Activer la journalisation pour INPUT (oui/non) ? : " log_input
     if [ "$log_input" == "oui" ]; then
         iptables -A INPUT -j LOG --log-prefix "INPUT BLOCK: "
+    elif [ "$log_input" == "non" ]; then
+    	 iptables -D INPUT -j LOG --log-prefix "INPUT BLOCK: "
     fi
     read -p "Activer la journalisation pour OUTPUT (oui/non) ? : " log_output
     if [ "$log_output" == "oui" ]; then
         iptables -A OUTPUT -j LOG --log-prefix "OUTPUT BLOCK: "
+    elif [ "$log_input" == "non" ]; then
+    	 iptables -D INPUT -j LOG --log-prefix "OUTPUT BLOCK: "
     fi
 }
+
+activer_journalisation
